214d9351f594ad5b31fda45ad2b787489d19596d
DanyloKuch <kucherudanylo@gmail.com>
2025-10-12 23:55:48 +0300

    Adding Api Controllers

---
+++ WebTechnologiesLab1/ApiControllers/ProductsApiController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebTechnologiesLab1.Data;
+using WebTechnologiesLab1.Models;   
+
+namespace WebTechnologiesLab1.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly WebDbContext _context;
+
+        public ProductsApiController(WebDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductsApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _context.Products
+                .Include(p => p.Category) 
+                .ToListAsync();
+
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/ProductsApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products
+                .Include(p => p.Category) 
+                .FirstOrDefaultAsync(p => p.id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        // POST: api/ProductsApi
+        [HttpPost]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetProduct", new { id = product.id }, product);
+        }
+
+        // PUT: api/ProductsApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProduct(int id, Product product)
+        {
+            if (id != product.id)
+            {
+                return BadRequest();
+            }
+            
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/ProductsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.id == id);
+        }
+
+    }
+}
--- WebTechnologiesLab1/Models/Category.cs
+++ WebTechnologiesLab1/Models/Category.cs
@@ -1,10 +1,13 @@
-﻿namespace WebTechnologiesLab1.Models
+﻿using System.Text.Json.Serialization;
+
+namespace WebTechnologiesLab1.Models
 {
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        [JsonIgnore]
         public ICollection<Product>? Products { get; set; }
     }
 }
